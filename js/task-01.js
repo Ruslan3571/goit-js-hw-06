784921a3f5e66fbf369a27b3fd6ed6fdc4c934f3
Ruslan3571 <ruslan12032@gmail.com>
2022-12-14 20:43:14 +0200

    5,8,9+

--- js/task-01.js
+++ js/task-01.js
@@ -2,10 +2,11 @@ const listEl = document.querySelector("#categories").children.length;
 console.log(`Number of categories: ${listEl}`);
 
 const newListEl = document.querySelectorAll(`.item`);
-console.log(newListEl);
+// console.log(newListEl);
 
 newListEl.forEach((ulEl) => {
   const titleEl = ulEl.firstElementChild.textContent;
   const liEl = ulEl.lastElementChild.children.length;
-  console.log(`Category: ${titleEl}; Elements: ${liEl}`);
+  console.log(`Category: ${titleEl}`);
+  console.log(`Elements: ${liEl}`);
 });
