605d063506ef54baf223b407bcfb35d19f6d96d4
Ruslan3571 <ruslan12032@gmail.com>
2022-12-14 21:13:11 +0200

    6,7+

---
+++ js/task-06.js
@@ -0,0 +1,39 @@
+document.getElementById("validation-input").onblur = function () {
+  console.log(this.value.length);
+  if (this.getAttribute("data-length") > this.value.length) {
+    this.classList.remove("valid");
+    this.classList.add("invalid");
+  } else {
+    this.classList.remove("invalid");
+    this.classList.add("valid");
+  }
+};
+
+// варіант 2
+// const inputEl = document.getElementById("validation-input");
+// const amountOfEl = inputEl.getAttribute("data-length");
+
+// inputEl.addEventListener("blur", function () {
+//   if (inputEl.value.length != amountOfEl) {
+//     inputEl.classList.remove("valid");
+//     inputEl.classList.add("invalid");
+//   } else {
+//     inputEl.classList.remove("invalid");
+//     inputEl.classList.add("valid");
+//   }
+// });
+
+// варіант 3
+
+// const input = document.querySelector("#validation-input");
+// const max = document.querySelector('input[data-length="6"]');
+
+// input.addEventListener("blur", () => {
+//   if (input.value.length !== Number(max.dataset.length)) {
+//     input.classList.remove("valid");
+//     input.classList.add("invalid");
+//   } else {
+//     input.classList.remove("invalid");
+//     input.classList.add("valid");
+//   }
+// });
